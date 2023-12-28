15652da41339acbddb9cac929244f73aaa175f07
Lubin Pappalardo <lubinpappalardo@gmail.com>
2023-12-28 18:56:59 -0500

    mobile fix

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,7 @@
 const logoElement = document.querySelector('#logo');
+const mobileNavigationPanel = document.querySelector('#mobile-navigation-panel');
+const mobileNavigationButton = document.querySelector('#mobile-navigation-btn');
+const mobileNavigationButtonClose = document.querySelector('#mobile-navigation-btn-close');
 
 logoElement.addEventListener('mouseover', () => {
     if (logoElement.classList.contains('hovered')) {
@@ -9,4 +12,12 @@ logoElement.addEventListener('mouseover', () => {
     setTimeout(() => {
         logoElement.classList.remove('hovered');
     }, 1000);
+});
+
+mobileNavigationButton.addEventListener('click', () => {
+    mobileNavigationPanel.classList.add('active');
+});
+
+mobileNavigationButtonClose.addEventListener('click', () => {
+    mobileNavigationPanel.classList.remove('active');
 });
